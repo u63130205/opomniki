2dcdc9c1198d72a4b29229c519c6a3668d453c4d
u63130205 <rr6789@student.uni-lj.si>
2016-03-10 14:29:00 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -9,6 +9,17 @@ window.addEventListener('load', function() {
 	}
 	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
 	
+	//dodaj opomnik
+	
+	var dodajOpomnik = function(){
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value = "";
+		document.querySelector("#opomniki").innerHTML += "<div class='opomnik senca rob'> <div class='naziv_opomnika'>" + naziv_opomnika + "</div><div class='cas_opomnika'>Opomnik čez <span>" + cas_opomnika + "</span> sekund.</div></div>"
+		
+	}
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
