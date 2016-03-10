782b2c271c9c1b2d81773dc9079cbf7f97f47fac
u63130205 <rr6789@student.uni-lj.si>
2016-03-10 13:59:44 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,6 +1,14 @@
 window.addEventListener('load', function() {
 	//stran nalozena
-		
+	
+	//Izvedi prijavo
+	var izvediPrijavo = function(){
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		document.querySelector(".pokrivalo").style.visibility = "hidden";
+	}
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
